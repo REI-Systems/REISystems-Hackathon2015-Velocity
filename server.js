3510c86528ab94e67a3b7c81bfee4a9e96e690e6
rsiruvuri-reisys <rsiruvuri@reisys.com>
2015-11-14 20:12:46 -0500

    added iis file

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-    console.log(req);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Velocity\n');
-}).listen(port);
